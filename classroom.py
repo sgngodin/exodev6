aed64254263d23b0e048694e933e9f3a170e19d9
MAC-i7 <mac-i7@MacBook-Pro-de-MAC-i7.local>
2021-03-24 13:36:25 +0100

    sans espace

--- classroom.py
+++ classroom.py
@@ -6,6 +6,6 @@ print("{} {}".format(stephane.prenom(), stephane.nom()))
 print("{} {}".format(Alexis.prenom(), Alexis.nom()))
 print("{} {}".format (quentin.prenom(), quentin.nom()))
 print("{} {}".format (gerard.Prenom(), gerard.Nom()))
-print ("{} {}".format (adrien.prenom(), adrien.nom()))
+print("{} {}".format (adrien.prenom(), adrien.nom()))
 print("{} {}".format(rémi.prenom(), rémi.nom()))
 
