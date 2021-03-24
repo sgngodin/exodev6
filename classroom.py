bd0cd6788be15d1f1dc10dfa463b6976f123b0d8
Your Name <you@example.com>
2021-03-24 14:00:13 +0100

    modif melinda

--- classroom.py
+++ classroom.py
@@ -1,9 +1,10 @@
 
-import stephane, Alexis, quentin
+import stephane, Alexis, quentin, Melinda
 
 print("Petit programme à 10")
 
 print("{} {}".format(stephane.prenom(), stephane.nom()))
 print("{} {}".format(Alexis.prenom(), Alexis.nom()))
-print("{} {}".format (quentin.prenom(), quentin.nom()))
+print("{} {}".format(quentin.prenom(), quentin.nom()))
+print("{} {}".format(Melinda.prenom(), Melinda.nom()))
 
