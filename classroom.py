9fc28ecf40cb030bc02019c6febc02018908f060
julien Dies <juliendies@gmail.com>
2021-03-24 12:17:06 +0100

    fichier classroom.py modifié par julien

--- classroom.py
+++ classroom.py
@@ -4,3 +4,4 @@ print ("petit programme à 10")
 
 print ("{} {}".format (stephane.prenom(), stephane.nom()))
 print("{} {}".format (quentin.prenom(), quentin.nom()))
+print("{} {}".format (julien.prenom(), julien.nom()))
