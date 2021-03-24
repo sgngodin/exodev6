5c43480f2ea1500f386ea47d965dc7b3b46c99b9
julien Dies <juliendies@gmail.com>
2021-03-24 13:36:44 +0100

    commit 2

--- classroom.py
+++ classroom.py
@@ -1,20 +1,19 @@
-<<<<<<< HEAD
-import stephane, Alexis, quentin, adrien, gerard, rémi
-=======
-import stephane, Alexis, quentin, adrien, julien
->>>>>>> julien
+
+import stephane, Alexis, quentin, adrien, gerard, rémi, julien
+
+
+
 
 print("Petit programme à 10")
 
 print("{} {}".format(stephane.prenom(), stephane.nom()))
 print("{} {}".format(Alexis.prenom(), Alexis.nom()))
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 print("{} {}".format(quentin.prenom(), quentin.nom()))
 print("{} {}".format(gerard.prenom(), gerard.nom()))
 print("{} {}".format(adrien.prenom(), adrien.nom()))
 print("{} {}".format(rémi.prenom(), rémi.nom()))
-=======
+
 print("{} {}".format (quentin.prenom(), quentin.nom()))
 
 print("{} {}".format (julien.prenom(), julien.nom()))
@@ -22,11 +21,11 @@ print("{} {}".format (julien.prenom(), julien.nom()))
 print ("{} {}".format (adrien.prenom(), adrien.nom()))
 
 
->>>>>>> julien
-=======
+
+
 print("{} {}".format (quentin.prenom(), quentin.nom()))
 print("{} {}".format (gerard.Prenom(), gerard.Nom()))
 print ("{} {}".format (adrien.prenom(), adrien.nom()))
 print("{} {}".format(rémi.prenom(), rémi.nom()))
 
->>>>>>> b689ca7b255f8398dfda62555f1144fb59aa6073
+
