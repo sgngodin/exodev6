f0d61434e9ff6718bcfe8fc0e1756001d9d6dcfc
Alexis <a_perotin@digital-campus.net>
2021-03-24 12:28:59 +0100

    Enlevage des conflits

--- classroom.py
+++ classroom.py
@@ -1,23 +1,10 @@
-<<<<<<< HEAD
-import stephane, Alexis, quentin, adrien, rémi
-=======
-import stephane, Alexis, quentin, adrien, gerard
-
->>>>>>> 1f7e2fa6f52bd1601983e06d112e0e1fc9c6d536
+import stephane, Alexis, quentin, adrien, gerard, rémi
 
 print("Petit programme à 10")
 
 print("{} {}".format(stephane.prenom(), stephane.nom()))
 print("{} {}".format(Alexis.prenom(), Alexis.nom()))
-print("{} {}".format (quentin.prenom(), quentin.nom()))
-<<<<<<< HEAD
-print("{} {}".format (gerard.prenom(), gerard.nom()))
-
-=======
-print ("{} {}".format (adrien.prenom(), adrien.nom()))
-<<<<<<< HEAD
-print ("{} {}".format (rémi.prenom(), rémi.nom()))
-=======
->>>>>>> 8f4b5e33d879ba4dd0e677a255aa0f88a80dbfb4
-
->>>>>>> 1f7e2fa6f52bd1601983e06d112e0e1fc9c6d536
+print("{} {}".format(quentin.prenom(), quentin.nom()))
+print("{} {}".format(gerard.prenom(), gerard.nom()))
+print("{} {}".format(adrien.prenom(), adrien.nom()))
+print("{} {}".format(rémi.prenom(), rémi.nom()))
