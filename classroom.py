7f739503b475c6f483efb68a0f58e85dafb23168
Alexis <a_perotin@digital-campus.net>
2021-03-24 11:58:25 +0100

    Quelques petites modif

--- classroom.py
+++ classroom.py
@@ -1,7 +1,6 @@
 import stephane, Alexis
 
-print ("petit programme à 10")
+print("Petit programme à 10")
 
-print ("{} {}".format (stephane.prenom(), stephane.nom()))
-print ("{} {}".format (Alexis.prenom(), Alexis.nom()))
-
+print("{} {}".format(stephane.prenom(), stephane.nom()))
+print("{} {}".format(Alexis.prenom(), Alexis.nom()))
