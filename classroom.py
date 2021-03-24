a573c5aecdbb4730478a602eb6d4cef60789dec0
MAC-i7 <mac-i7@MacBook-Pro-de-MAC-i7.local>
2021-03-24 13:44:48 +0100

    dernière modif

--- classroom.py
+++ classroom.py
@@ -8,24 +8,11 @@ print("Petit programme à 10")
 
 print("{} {}".format(stephane.prenom(), stephane.nom()))
 print("{} {}".format(Alexis.prenom(), Alexis.nom()))
-
 print("{} {}".format(quentin.prenom(), quentin.nom()))
-print("{} {}".format(gerard.prenom(), gerard.nom()))
+print("{} {}".format(gerard.Prenom(), gerard.Nom()))
 print("{} {}".format(adrien.prenom(), adrien.nom()))
 print("{} {}".format(rémi.prenom(), rémi.nom()))
-
-print("{} {}".format (quentin.prenom(), quentin.nom()))
-
 print("{} {}".format (julien.prenom(), julien.nom()))
 
-print ("{} {}".format (adrien.prenom(), adrien.nom()))
-
-
-
-
-print("{} {}".format (quentin.prenom(), quentin.nom()))
-print("{} {}".format (gerard.Prenom(), gerard.Nom()))
-print("{} {}".format (adrien.prenom(), adrien.nom()))
-print("{} {}".format(rémi.prenom(), rémi.nom()))
 
 
