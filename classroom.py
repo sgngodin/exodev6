2386e7e26f9e2adecfb95e519de9e087541083e1
MAC-i7 <mac-i7@MacBook-Pro-de-MAC-i7.local>
2021-03-24 12:31:08 +0100

    commit xxx

--- classroom.py
+++ classroom.py
@@ -6,10 +6,7 @@ print("Petit programme à 10")
 print("{} {}".format(stephane.prenom(), stephane.nom()))
 print("{} {}".format(Alexis.prenom(), Alexis.nom()))
 print("{} {}".format (quentin.prenom(), quentin.nom()))
-<<<<<<< HEAD
-print("{} {}".format (gerard.prenom(), gerard.nom()))
-
-=======
+print("{} {}".format (gerard.Prenom(), gerard.Nom()))
 print ("{} {}".format (adrien.prenom(), adrien.nom()))
->>>>>>> 8f4b5e33d879ba4dd0e677a255aa0f88a80dbfb4
+
 
